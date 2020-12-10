b87a3b9c582f6b0124947ff306e6e419ffcc617c
bweerasakoutlook <bweerasak@outlook.com>
2020-12-10 10:44:23 +0700

    complete layout with row and column

--- lib/pages/home_stack/about_page.dart
+++ lib/pages/home_stack/about_page.dart
@@ -13,7 +13,6 @@ class _AboutPageState extends State<AboutPage> {
     return Scaffold(
       appBar: AppBar(
         title: Text('About'),
-      
       ),
       body: SingleChildScrollView(
         child: Column(
@@ -22,55 +21,98 @@ class _AboutPageState extends State<AboutPage> {
           children: [
             Image.asset('assets/images/anime_club.jpg'),
             SafeArea(
-              child: Padding(
-                padding: EdgeInsets.all(10),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('TOT Public company limited', 
-                    style:TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                child: Padding(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'TOT Public company limited',
+                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold,
+                      color: Colors.orangeAccent,
                     ),
-                    Divider(),
-                    Text('Dart is a client-optimized programming language for apps on multiple platforms. It is developed by Google and is used to build mobile,'),
-                    Divider(),
-                    SizedBox(height:20),
-                    Row(
-                      children: [
-                        Column(
-                          children: [
-                            Icon(Icons.wb_sunny, color: Colors.orange,),
-                          ],
-                        ),
-                        SizedBox(width: 40),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Row(children: [
-                              Text('TOT Academy', 
-                              style: TextStyle(
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold, 
-                                color: Colors.orange
-                                ),
+                  ),
+                  Divider(),
+                  Text(
+                      'Dart is a client-optimized programming language for apps on multiple platforms. It is developed by Google and is used to build mobile,'),
+                  Divider(),
+                  SizedBox(height: 20),
+                  Row(
+                    children: [
+                      Column(
+                        children: [
+                          Icon(
+                            Icons.wb_sunny,
+                            color: Colors.orange,
+                          ),
+                        ],
+                      ),
+                      SizedBox(width: 40),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            children: [
+                              Text(
+                                'TOT Academy',
+                                style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.orange),
                               ),
-                            ],),
-                            Row(children: [
+                            ],
+                          ),
+                          Row(
+                            children: [
                               Text('Maung Khonkaen'),
-                            ],),
-                          ],
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  Divider(),
+
+                    
+                  Wrap(
+                   alignment: WrapAlignment.center,
+                    spacing: 5,
+                    children: List.generate(8,(index) => 
+                      Chip(
+                          label: Text('${index + 1} Person'),
+                          avatar: Icon(Icons.person_add),
+                          backgroundColor: Colors.blueAccent,
                         ),
-                      ],
+                      ),
+                    
+                  ),
+                Divider(),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    CircleAvatar(backgroundImage: AssetImage('assets/images/OIP_5.jpg'),radius: 30,),
+                    CircleAvatar(backgroundImage: AssetImage('assets/images/OIP_3.jpg'),radius: 30,),
+                    CircleAvatar(backgroundImage: AssetImage('assets/images/dark_png.png'),radius: 30,),
+                    SizedBox(
+                      width: 70,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Icon(Icons.track_changes, color: Colors.deepOrangeAccent,),
+                          Icon(Icons.train, color: Colors.deepOrange,),
+                          Icon(Icons.wifi, color: Colors.orangeAccent,),
+                        ],
+                      ),
                     ),
                     Divider(),
-                    
                   ],
-
                 ),
-                )
+                ],
               ),
+            )),
           ],
         ),
-        ),
-      );
+      ),
+    );
   }
 }

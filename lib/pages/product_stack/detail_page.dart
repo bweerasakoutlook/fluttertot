f0b681c40c4afd2f12d156f3f105c90b28181026
bweerasakoutlook <bweerasak@outlook.com>
2020-12-20 14:32:13 +0700

    detail page with future builder

--- lib/pages/product_stack/detail_page.dart
+++ lib/pages/product_stack/detail_page.dart
@@ -13,21 +13,14 @@ class _DetailPageState extends State<DetailPage> {
   Map<String, dynamic> product;
   Map<String, dynamic> detail;
 
-  Future<void> getData(int id) async {
+  Future<dynamic> getData(int id) async {
     var url = 'https://api.codingthailand.com/api/course/$id';
     var response = await http.get(url);
     if (response.statusCode == 200) {
       detail = json.decode(response.body);
-      print(detail['data']);
-      // setState(() {
-      //   data = detail['data'];
-      //   isLoading = false;
-      // });
+      return detail;
     } else {
-      // setState(() {
-      //   isLoading = false;
-      // });
-      print('error from backend 400/500');
+      throw Exception('error from backend code 400/500');
     }
   }
 
@@ -35,13 +28,33 @@ class _DetailPageState extends State<DetailPage> {
   Widget build(BuildContext context) {
     product = ModalRoute.of(context).settings.arguments;
 
-    getData(product['id']);
-
     return Scaffold(
       appBar: AppBar(
         title: Text('${product['title']}'),
       ),
-      body: Text('${product['title']}'),
+      body: FutureBuilder<dynamic>(
+        future: getData(product['id']),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.separated(
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text('${snapshot.data['data'][index]['ch_title']}'),
+                    subtitle: Text('${snapshot.data['data'][index]['ch_dateadd']}'),
+                    trailing: Chip(
+                      label: Text('${snapshot.data['data'][index]['ch_view']}'),
+                      backgroundColor: Colors.orange[300],
+                    ),
+                  );
+                },
+                separatorBuilder: (context, index) => Divider(),
+                itemCount: snapshot.data['data'].length);
+          } else if (snapshot.hasError) {
+            Text('have any error ${snapshot.error}');
+          }
+          return Center(child: CircularProgressIndicator());
+        },
+      ),
     );
   }
 }
